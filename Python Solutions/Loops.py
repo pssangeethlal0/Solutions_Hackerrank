d4461211232107e5fd189f5122eb318360d578d6
Sandeep S Lopez <55293010+sslopez@users.noreply.github.com>
2020-10-14 19:48:37 +0530

    added Loops

---
+++ Python Solutions/Loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+if(2<=n<=20):
+    for i in range(1,11):
+        print(n,"x",i,"=",n*i)
+  
